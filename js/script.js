e0e6c6ba7bf263e7619e53a42f32bc48af961218
Antonio Carlos <carlos_antonio.minga@outlook.com>
2023-04-13 09:03:43 -0300

    configurações scrollreveal

---
+++ js/script.js
@@ -0,0 +1,14 @@
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 500
+}).reveal(`
+.container,
+.image-wrapper, 
+.profile-picture,
+.profile-username,
+.profile-description,
+.list-items,
+.list-item,
+.list-item a,
+.footer-link`);
